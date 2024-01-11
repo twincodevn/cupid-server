749ca9d5475fb06b5d7831a9126d84639d1dd1fc
Bravee06 <bravee300@gmail.com>
2024-01-11 09:19:48 +0700

    initial project

---
+++ src/main/java/com/cupidconnect/CupidServerApplication.java
@@ -0,0 +1,13 @@
+package com.cupidconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupidServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupidServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cupidconnect/CupidServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupidconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupidServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
